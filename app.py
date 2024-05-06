3b9d60ae9f93aa63136454dbc2cf9d27a8ce7500
Sowmya AM <2260927@cognizant.com>
2024-05-06 16:18:13 +0530

    added app

---
+++ app.py
@@ -0,0 +1,11 @@
+import os 
+import sys 
+import boto3 
+import streamlit
+
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain.llms.bedrock import Bedrock
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import retrieval_qa
+from langchain.vectorstores import FAISS 
